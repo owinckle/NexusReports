67cb40e2088a471611a3e33c19e22a384b3f6127
Ocean <oceanwinckler.rem@gmail.com>
2023-01-20 10:26:09 +0100

    init

---
+++ src/main/java/me/yukinox/nexusreports/nexusreports/NexusReports.java
@@ -0,0 +1,17 @@
+package me.yukinox.nexusreports.nexusreports;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NexusReports extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
